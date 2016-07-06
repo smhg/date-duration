3ca40d483a101ecc555b522d0328d46a67e9d7b7
smhg <sam@strictlyphp.com>
2016-07-06 19:03:43 +0200

    make year, month and day DST independent

--- test/duration.js
+++ test/duration.js
@@ -22,11 +22,41 @@ describe('createDuration', () => {
 
   describe('#addTo', () => {
     it('should add the duration to a date', () => {
-      const date = new Date('2015-10-15T01:00:00.000Z');
+      assert.equal(
+        createDuration('PT1H')
+          .addTo(new Date('2015-10-15T01:00:00.000Z'))
+          .toISOString(),
+        '2015-10-15T02:00:00.000Z'
+      );
 
-      assert.equal(createDuration('PT1H').addTo(date).toISOString(), '2015-10-15T02:00:00.000Z');
+      assert.equal(
+        createDuration('P1W')
+          .addTo(new Date('2015-12-15T02:00:00.000Z'))
+          .toISOString(),
+        '2015-12-22T02:00:00.000Z'
+      );
+    });
 
-      assert.equal(createDuration('P1W').addTo(new Date('2015-12-15T02:00:00.000Z')).toISOString(), '2015-12-22T02:00:00.000Z');
+    it('should not affect underlying units', () => {
+      assert.deepEqual(
+        createDuration('P1D')
+          .addTo(new Date(2016, 9, 30, 0, 0, 0)),
+        new Date(2016, 9, 31, 0, 0, 0)
+      );
+    });
+
+    it('should', () => {
+      assert.deepEqual(
+        createDuration('PT1H')
+          .addTo(new Date('Sun Oct 30 2016 03:00:00 GMT+0200 (CEST)')),
+        new Date('Sun Oct 30 2016 03:00:00 GMT+0100 (CET)')
+      );
+
+      assert.deepEqual(
+        createDuration('PT24H')
+          .addTo(new Date('Sun Oct 30 2016 01:00:00 GMT+0200 (CEST)')),
+        new Date('Mon Oct 31 2016 00:00:00 GMT+0100 (CET)')
+      );
     });
 
     it('should add the duration to a date-like object', () => {
@@ -42,11 +72,41 @@ describe('createDuration', () => {
 
   describe('#subtractFrom', () => {
     it('should subtract the duration from a date', () => {
-      const date = new Date('2015-10-25T03:00:00.000Z');
+      assert.equal(
+        createDuration('PT1H')
+          .subtractFrom(new Date('2015-10-25T03:00:00.000Z'))
+          .toISOString(),
+        '2015-10-25T02:00:00.000Z'
+      );
 
-      assert.equal(createDuration('PT1H').subtractFrom(date).toISOString(), '2015-10-25T02:00:00.000Z');
+      assert.equal(
+        createDuration('P2W')
+          .subtractFrom(new Date('2015-12-15T02:00:00.000Z'))
+          .toISOString(),
+        '2015-12-01T02:00:00.000Z'
+      );
+    });
 
-      assert.equal(createDuration('P2W').subtractFrom(new Date('2015-12-15T02:00:00.000Z')).toISOString(), '2015-12-01T02:00:00.000Z');
+    it('should not affect underlying units', () => {
+      assert.deepEqual(
+        createDuration('P1D')
+          .subtractFrom(new Date(2016, 9, 31, 0, 0, 0)),
+        new Date(2016, 9, 30, 0, 0, 0)
+      );
+    });
+
+    it('should', () => {
+      assert.deepEqual(
+        createDuration('PT1H')
+          .subtractFrom(new Date('Sun Oct 30 2016 03:00:00 GMT+0100 (CET)')),
+        new Date('Sun Oct 30 2016 03:00:00 GMT+0200 (CEST)')
+      );
+
+      assert.deepEqual(
+        createDuration('PT24H')
+          .subtractFrom(new Date('Mon Oct 31 2016 00:00:00 GMT+0100 (CET)')),
+        new Date('Sun Oct 30 2016 01:00:00 GMT+0200 (CEST)')
+      );
     });
 
     it('should subtract the duration from a date-like object', () => {
