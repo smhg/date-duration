f7b3f42526aa9bc10b4bcf5655bd4d9b11b3c99e
smhg <sam.hauglustaine@gmail.com>
2017-10-01 10:16:42 +0200

    use tagged template literals to generate iso string

--- src/duration.js
+++ src/duration.js
@@ -16,6 +16,12 @@ const clone = value => {
 
   return new Date(+value);
 };
+const stripEmptyElements = ([, ...elements], ...values) =>
+  elements.reduce(
+    (result, element, index) =>
+      `${result}${Number.isInteger(values[index]) ? `${values[index]}${element}` : ''}`,
+    ''
+  );
 const parser = /P(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)W)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?/;
 const dateMethods = new Map([
   ['year', 'FullYear'],
@@ -48,11 +54,11 @@ export default function createDuration (iso) {
 
   return Object.freeze({
     toString: () =>
-      `P${(year !== undefined ? `${year}Y` : '')}${(month !== undefined ? `${month}M` : '')}${(week !== undefined ? `${week / 7}W` : '')}${(day !== undefined ? `${day}D` : '')}${
-        (hour !== undefined || minute !== undefined || second !== undefined
-          ? `T${(hour !== undefined ? `${hour}H` : '')}${(minute !== undefined ? `${minute}M` : '')}${(second !== undefined ? `${second}S` : '')}`
-          : ''
-        )}`,
+      `P${
+        stripEmptyElements`${year}Y${month}M${week / 7}W${day}D`
+      }T${
+        stripEmptyElements`${hour}H${minute}M${second}S`
+      }`.replace(/T$/, ''),
     addTo: date => {
       let d = clone(date);
 
